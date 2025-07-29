3efe87273aeefa8193aa4491dcb02e8852f4107e
OmPrakash Rathore <omr31997@gmail.com>
2025-07-29 05:50:06 +0000

    Update With Something

---
+++ Sqr_Pattern.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nTimes=3;
+
+    /* *Start Pattern* */
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<nTimes; j++)
+    //     {
+    //         cout << '*';
+    //     }
+    //     cout<<"\n";
+    // }
+
+    /* *Numbers* */
+    // for(int i=0; i<nTimes;i++)
+    // {
+    //     for(int j=0; j<nTimes; j++)
+    //     {
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout <<endl;
+    // }
+
+    // int num=1;
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<nTimes; j++)
+    //     {
+    //         cout <<num<<' ';
+    //         num++;
+    //     }
+    //     cout <<endl;
+    // }
+
+    /* *Alphabet* */
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     char ch ='A';
+    //     for(int j=0; j<nTimes; j++)
+    //     {
+    //         cout <<ch<<" ";
+    //         ch=ch+1; //65 + 1 = 66
+    //     }
+    //     cout <<endl;
+    // }
+
+    // char ch = 'A';
+
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<nTimes; j++)
+    //     {
+    //         cout <<ch<<' ';
+    //         ch = ch+1;
+    //     }
+    //     cout <<endl;
+    // }
+}
---
+++ Traingle_Pattern.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nTimes = 6;
+
+    /* *Star Pattern* */
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<i+1; j++)   //when j becomes i+1 < i+1 will be geting false
+    //     {
+    //         cout <<"* ";
+    //     }
+    //     cout <<endl;
+    // }
+
+    /* *Number* */
+    // for(int i=0; i<nTimes;i++)
+    // {
+    //     for(int j=0; j<i+1; j++)
+    //     {
+    //         cout <<(i+1)<<' ';
+    //     }
+    //     cout <<endl;
+    // }
+
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=1; j<=i+1; j++)
+    //     {
+    //         cout <<j<<' ';
+    //     }
+    //     cout<<endl;
+    // }
+
+    /* *Reverse Number* */
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=i+1; j>0; j--)
+    //     {
+    //         cout << j<<' ';
+    //     }
+    //     cout <<endl;
+    // }
+
+    /* *Foly's Triangle Pattern* */
+
+    
+    /* *Alphabet* */
+    // char ch = 'A';
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<i+1; j++)
+    //     {
+    //         cout <<ch<<' ';
+    //     }
+
+    //     ch = ch+1;
+    //     cout <<endl;
+    // }
+    return 0;
+}
