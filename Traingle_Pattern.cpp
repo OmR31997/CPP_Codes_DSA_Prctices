cceeced62b7246d816ed209abccea0a3d1980531
OmPrakash Rathore <omr31997@gmail.com>
2025-07-29 08:16:21 +0000

    Update CPP

--- Traingle_Pattern.cpp
+++ Traingle_Pattern.cpp
@@ -26,28 +26,6 @@ int main()
     //     cout <<endl;
     // }
 
-    // for(int i=0; i<nTimes; i++)
-    // {
-    //     for(int j=1; j<=i+1; j++)
-    //     {
-    //         cout <<j<<' ';
-    //     }
-    //     cout<<endl;
-    // }
-
-    /* *Reverse Number* */
-    // for(int i=0; i<nTimes; i++)
-    // {
-    //     for(int j=i+1; j>0; j--)
-    //     {
-    //         cout << j<<' ';
-    //     }
-    //     cout <<endl;
-    // }
-
-    /* *Foly's Triangle Pattern* */
-
-    
     /* *Alphabet* */
     // char ch = 'A';
     // for(int i=0; i<nTimes; i++)
@@ -60,5 +38,108 @@ int main()
     //     ch = ch+1;
     //     cout <<endl;
     // }
+
+    //--------------------------------------------------------
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=1; j<=i+1; j++)
+    //     {
+    //         cout <<j<<' ';
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // for(int i =0; i<nTimes; i++)
+    // {
+    //     char ch='A';
+    //     for(int j=1; j<=i+1; j++)
+    //     {
+    //         cout <<ch<<' ';
+    //         ch++;
+    //     }
+    //     cout <<endl;
+    // }
+
+    //---------------------------------------------------
+    /* *Reverse Number* */
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=i+1; j>0; j--)
+    //     {
+    //         cout << j<<' ';
+    //     }
+    //     cout <<endl;
+    // }
+
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     char ch = 'A';
+    //     for(int j=i+1; j>0; j--)
+    //     {
+    //         char temp = ch + j - 1;
+    //         cout <<temp<<' ';
+    //     }
+    //     cout <<'\n';
+    // }
+//------------------------------------------------------
+    /* *Foly's Triangle Pattern* */
+    // int num=1;
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<i+1; j++)
+    //     {
+    //         cout << num<<' ';
+    //         num++;
+    //     }
+    //     cout <<'\n';
+    // }
+
+    // char ch = 'A';
+
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<i+1; j++)
+    //     {
+    //         cout<<ch<<' ';
+    //         ch++;
+    //     }
+    //     cout <<'\n';
+    // }
+    
+//-----------------------------------------------------------
+
+    /* *Invert Right Triangle* */
+    // for(int i=0; i<=nTimes; i++)
+    // {
+    //     for(int j=0; j<i; j++)
+    //     {
+    //         cout <<' ';
+    //     }
+
+    //     for(int k=0; k<nTimes-i; k++)
+    //     {
+    //         cout <<(i+1);
+    //     }
+    //     cout <<'\n';
+    // }
+
+    // char ch='A';
+    // for(int i=0; i<nTimes; i++)
+    // {
+    //     for(int j=0; j<i+1; j++)
+    //     {
+    //         cout <<' ';
+    //     }
+
+    //     for(int k=0; k<nTimes-i; k++)
+    //     {
+    //         cout <<ch;
+    //     }
+    //     cout <<'\n';
+    //     ch++;
+    // }
+
+    
     return 0;
 }
